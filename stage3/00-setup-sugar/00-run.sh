a671a20d95312984d391b3c30c40e355ea45ab45
44yu5h <meet44yu5h@gmail.com>
2024-09-22 05:12:16 +0530

    mvp test04
    What changes did I make?
    - Ran the `dpkg-reconfigure lightdm` command in the chroot environment and switched the mode from CLI to GUI (B3)
    
    Results:
    It was a failure.
    - The lightdm seemed to be configured but failed to start.
    - Logs: Error: "... Failed to start lightdm.service - Light Display Manager. ..."
    
    What are the possibilities?
    - We are doing what we did traditionally to install sugar on rpi. Maybe the RPi Lite OS built in stage2 is not the same as the official one we used. (i.e. missing some additional packages)
    
    Will try to have a bare RPi Lite OS at stage2 and manually install Sugar as we did traditionally in test05

--- stage3/00-setup-sugar/00-run.sh
+++ stage3/00-setup-sugar/00-run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-dpkg-reconfigure lightdm
-sudo raspi-config nonint do_boot_behaviour B3
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" dpkg-reconfigure lightdm
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B3
+EOF
+
 mkdir ${ROOTFS_DIR}/home/pi/Activities
 cd ${ROOTFS_DIR}/home/pi/Activities
 git clone https://github.com/44yu5h/gallery_activity.git
-echo "###### Finished 01-run.sh #####"
+echo "###### Finished 00-run.sh #####"
