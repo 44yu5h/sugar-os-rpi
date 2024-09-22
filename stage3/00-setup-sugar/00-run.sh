af55546b308885711bd806506d69792cc7857407
44yu5h <meet44yu5h@gmail.com>
2024-09-22 23:50:58 +0530

    mvp test13 (working)
    It is the 13th test I believe, stopped counting after 6.
    Anyway I tried several things, including
    - manually trying to setup lightdm (i.e. what dpkg-reconfigure does)
    - pulling apart the steps involved and re-doing it one by one automatically and partially manually
    - pulling my hair for hours
    
    It was working fine when all steps were done manually but not when I used the script to do THE SAME THING
    After several more tries I found the problem was under my nose.
    
    what was the issue?
    - the lightdm was being installed but NOT correctly (/in the same way) when installed from `00-packages` than when used apt to do so.
    
    fix?
    - used chroot ennvironment to directly install the packages and setup sugar.
    
    There goes my 30 hrs on this "beautiful" quirk :)

--- stage3/00-setup-sugar/00-run.sh
+++ stage3/00-setup-sugar/00-run.sh
@@ -1,13 +1,25 @@
 #!/bin/bash -e
 
-sudo dpkg-reconfigure lightdm
-sudo raspi-config nonint do_boot_behaviour B3
+on_chroot << EOF
+sudo -u root apt install lightdm xserver-xorg sucrose -y
+sudo -u root dpkg-reconfigure lightdm
+sudo -u root raspi-config nonint do_boot_behaviour B3
+EOF
+
+# Set the default session to Sugar
+CONFIG_FILE="/etc/lightdm/lightdm.conf"
+if grep -q "\[Seat:\*\]" "$CONFIG_FILE"; then
+  sudo sed -i '/^\[Seat:\*\]/a user-session=sugar' "$CONFIG_FILE"
+else
+  echo -e "[Seat:*]\nuser-session=sugar" | sudo tee -a "$CONFIG_FILE"
+fi
+echo "Default session set to Sugar."
 
 mkdir ${ROOTFS_DIR}/home/pi/Activities
 cd ${ROOTFS_DIR}/home/pi/Activities
-git clone https://github.com/44yu5h/gallery_activity.git
-echo "###### Finished 00-run.sh #####"
+# clone Activities here
+echo "Sugar is successfully installed."
 
-echo "enabling ipv6.."
+# re-enabling ipv6..
 sudo sed -i '/net\.ipv6\.conf\.\(all\|default\|lo\)\.disable_ipv6 = 1/d' /etc/sysctl.conf
 sudo sysctl -p
--- stage3/prerun.sh
+++ stage3/prerun.sh
@@ -4,6 +4,6 @@ if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
 
-echo "disabling ipv6.."
+# disabling ipv6 for now..
 sudo bash -c 'echo -e "\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf'
 sudo sysctl -p
