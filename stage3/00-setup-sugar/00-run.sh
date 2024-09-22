6505f34cea22e3ea3678dad46ad324bf90d55e9a
44yu5h <meet44yu5h@gmail.com>
2024-09-22 09:46:20 +0530

    mvp test05
    What did u dooooo?
    - Removed the setup script and packages
    - Manually install sucrose and setup lightdm
    
    Did it workkkk?
    - Yepp!
    
    Did u spot the errorrrr?
    - ipv6 (~_~)
    
    Fix? Temporarily disable ipv6 and then re-enable when done.
    (I can't say if it is my wifi that's causing the issue but nevertheless disabling ipv6 does no harm either way)
    Will implement these in the next commit and test again
    
    how?
    add some lines to disable ipv6 in `/etc/sysctl.conf`:
    ```
    net.ipv6.conf.all.disable_ipv6 = 1
    net.ipv6.conf.default.disable_ipv6 = 1
    net.ipv6.conf.lo.disable_ipv6 = 1
    ```

--- stage3/00-setup-sugar/00-run.sh
+++ stage3/00-setup-sugar/00-run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" dpkg-reconfigure lightdm
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B3
-EOF
-
 mkdir ${ROOTFS_DIR}/home/pi/Activities
 cd ${ROOTFS_DIR}/home/pi/Activities
 git clone https://github.com/44yu5h/gallery_activity.git
