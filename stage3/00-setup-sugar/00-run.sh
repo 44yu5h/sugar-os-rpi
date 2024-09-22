f359b25155178f1e212ddb172d905198b31946ea
44yu5h <meet44yu5h@gmail.com>
2024-09-22 20:05:39 +0530

    mvp test06
    Results:
    aghhhhh! why!?
    
    - So the download completes fine but the lightdm still doesn't get configured - Error: lightdm is not installed
    - I think the command is ran on my RPi instead of the OS being built

--- stage3/00-setup-sugar/00-run.sh
+++ stage3/00-setup-sugar/00-run.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
+
+sudo dpkg-reconfigure lightdm
+sudo raspi-config nonint do_boot_behaviour B3
 
 mkdir ${ROOTFS_DIR}/home/pi/Activities
 cd ${ROOTFS_DIR}/home/pi/Activities
 git clone https://github.com/44yu5h/gallery_activity.git
 echo "###### Finished 00-run.sh #####"
+
+echo "enabling ipv6.."
+sudo sed -i '/net\.ipv6\.conf\.\(all\|default\|lo\)\.disable_ipv6 = 1/d' /etc/sysctl.conf
+sudo sysctl -p
--- stage3/prerun.sh
+++ stage3/prerun.sh
@@ -3,3 +3,7 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
+
+echo "disabling ipv6.."
+sudo bash -c 'echo -e "\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf'
+sudo sysctl -p
